0223c24f1b51f7c9a122db902d374cf7dd8b9d04
xeniasuper <ichmalegern@gmail.com>
2019-06-02 13:32:11 +0300

    Updates

---
+++ precache-manifest.6a9967314bf2e76492b64b50bee449d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36305fd8b0406b4dc49e25b4e09c2299",
+    "url": "/trello-board/index.html"
+  },
+  {
+    "revision": "d996eb4d44295224e986",
+    "url": "/trello-board/static/css/main.a2ea8286.chunk.css"
+  },
+  {
+    "revision": "d27dcead68faee6d2b37",
+    "url": "/trello-board/static/js/2.44949046.chunk.js"
+  },
+  {
+    "revision": "d996eb4d44295224e986",
+    "url": "/trello-board/static/js/main.3b072480.chunk.js"
+  },
+  {
+    "revision": "3d6445447c08826ee70c",
+    "url": "/trello-board/static/js/runtime~main.c0bdb7f7.js"
+  }
+]);
